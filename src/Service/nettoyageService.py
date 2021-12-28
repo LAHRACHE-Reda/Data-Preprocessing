3063f44a6bc0c0c8fba6982a2fd8b9f83c9e7f27
imane-badda <imanebadda987@gmail.com>
2021-12-28 21:57:12 +0100

    infinit values features

--- src/Service/nettoyageService.py
+++ src/Service/nettoyageService.py
@@ -1,4 +1,8 @@
 # Identifier les valeurs null
+import sys
+
+import numpy as np
+
 from src import app
 
 
@@ -9,3 +13,19 @@ def identify_null_values():
 # Supprimer les valeurs null
 def drop_null_values():
     app.config['dataset'].dropna(inplace=True)
+
+
+#Identify the infinit values
+def indentify_infinit_values():
+    return app.config['dataset'][app.config['dataset'].isin([np.inf, -np.inf]).any(axis=1)]
+
+#Delete infinit values
+def drop_infinit_values():
+    index = app.config['dataset'].index
+    condition = app.config['dataset'].isin([np.inf, -np.inf]).any(axis=1)
+    indices = index[condition]
+    indices_list = indices.tolist()
+    for i in indices_list:
+        app.config['dataset'].drop(index=i, inplace=True)
+    return len(indices_list)
+
--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,11 @@
 # create the flask app
 import pandas as pd
 from flask import Flask
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['UPLOAD_PATH'] = "./csvFiles"
 app.config['dataset'] = pd.DataFrame()
