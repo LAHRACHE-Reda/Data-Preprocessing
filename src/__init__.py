8b2261ef63fe98cf5d759497c5ff4f7f35c2087f
LAHRACHE Reda <58349137+LAHRACHE-Reda@users.noreply.github.com>
2022-01-27 18:44:36 +0100

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -2,10 +2,7 @@
 import pandas as pd
 from flask import Flask
 from flask_cors import CORS
-<<<<<<< HEAD
 
-=======
->>>>>>> 547f5ff8248f652b6b391f003d012f7ad2d3e26c
 
 app = Flask(__name__)
 CORS(app)
@@ -16,3 +13,5 @@ app.config['dataset'] = pd.DataFrame()
 # Déclaration des controllers
 import src.Controller.uploadController
 import src.Controller.nettoyageController
+import src.Controller.dashboardController
+import src.Controller.exportController
